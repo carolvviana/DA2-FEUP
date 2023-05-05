b1762f207adb170684d406b1c58dfae63352f21b
Sérgio Peixoto <sergiopeixoto3@gmail.com>
2023-05-05 15:18:23 +0100

    added graph class

---
+++ src/TSP.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Sergio on 05/05/2023.
+//
+
+#include "TSP.h"
---
+++ src/TSP.h
@@ -0,0 +1,14 @@
+//
+// Created by Sergio on 05/05/2023.
+//
+
+#ifndef PROJETO_DA_2_TSP_H
+#define PROJETO_DA_2_TSP_H
+
+
+class TSP {
+
+};
+
+
+#endif //PROJETO_DA_2_TSP_H
